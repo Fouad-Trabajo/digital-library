fcdbbf414e491d1bf128eceb7af3c5a7ebfb30f2
Fouad (Fuaa) Aharchi Aharchi <146753676+Fouad-Trabajo@users.noreply.github.com>
2024-04-28 05:03:52 +0200

    Dar de alta a un usuario en el sistema (código encargado sobre la funcionalidad de dar de alta) (#2)
    * feat: Parte del código encargada de dar de alta a un usuario en el sistema
    
    * feat: Parte del código encargada de dar de alta a un usuario en el sistema

---
+++ src/main/java/com/library/feature/user/domain/User.java
@@ -0,0 +1,25 @@
+package com.library.feature.user.domain;
+
+public class User {
+
+    public final String id, name, surname, dni, dateInscription;
+
+    public User(String id, String name, String surname, String dni, String dateInscription) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.dni = dni;
+        this.dateInscription = dateInscription;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", dni='" + dni + '\'' +
+                ", dateInscription='" + dateInscription + '\'' +
+            '}';
+    }
+}
---
+++ src/main/java/com/library/feature/user/domain/UserRepository.java
@@ -0,0 +1,5 @@
+package com.library.feature.user.domain;
+
+public interface UserRepository {
+    void createUser(User user);
+}

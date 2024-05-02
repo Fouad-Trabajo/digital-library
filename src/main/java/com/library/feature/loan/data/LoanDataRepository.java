03385fd275a0f4248598d3608acbdbc94a91fe30
Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>
2024-05-02 21:50:23 +0200

    feat: Mostrar listado de préstamos activos

--- src/main/java/com/library/feature/loan/data/LoanDataRepository.java
+++ src/main/java/com/library/feature/loan/data/LoanDataRepository.java
@@ -4,6 +4,8 @@ import com.library.feature.loan.data.local.LoanFileLocalDataSource;
 import com.library.feature.loan.domain.Loan;
 import com.library.feature.loan.domain.LoanRepository;
 
+import java.util.List;
+
 public class LoanDataRepository implements LoanRepository {
 
     private LoanFileLocalDataSource loanFileLocalDataSource;
@@ -20,5 +22,10 @@ public class LoanDataRepository implements LoanRepository {
     @Override
     public void deleteLoan(String id) {
         loanFileLocalDataSource.delete(id);
+    }
+
+    @Override
+    public List<Loan> getLoans() {
+        return loanFileLocalDataSource.findAll();
     }
 }
--- src/main/java/com/library/feature/loan/domain/Loan.java
+++ src/main/java/com/library/feature/loan/domain/Loan.java
@@ -1,5 +1,6 @@
 package com.library.feature.loan.domain;
 
+import com.library.feature.digitalresources.domain.DigitalBook;
 import com.library.feature.digitalresources.domain.DigitalResources;
 import com.library.feature.user.domain.User;
 
@@ -7,10 +8,10 @@ public class Loan {
 
     public final String id, startDate, endDate, laonStatus;
     public User user;
-    public DigitalResources digitalBook;
+    public DigitalBook digitalBook;
 
     public Loan(String id, String startDate, String endDate, String laonStatus, User user,
-                DigitalResources digitalBook) {
+                DigitalBook digitalBook) {
         this.id = id;
         this.startDate = startDate;
         this.endDate = endDate;
@@ -21,13 +22,13 @@ public class Loan {
 
     @Override
     public String toString() {
-        return "LOAN{" +
+        return "Loan{" +
                 "id='" + id + '\'' +
                 ", startDate='" + startDate + '\'' +
                 ", endDate='" + endDate + '\'' +
                 ", laonStatus='" + laonStatus + '\'' +
-                ", USER=" + user +
-                ", DIGITALBOOK=" + digitalBook +
+                ", user=" + user +
+                ", digitalBook=" + digitalBook +
                 '}';
     }
 }
